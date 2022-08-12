7f78dd36427b907270aa24d3c2917eaeec3bcfb8
Jaedson Silva <contacanal798@gmail.com>
2022-08-12 13:03:11 -0300

    changing password request in main function

--- cookiedbcli/main.py
+++ cookiedbcli/main.py
@@ -12,18 +12,28 @@ def main():
     dbcli = CookieDBCLI()
     databases = dbcli.get_databases()
 
-    while True:
-        password = getpass.getpass('Password: ')
-        dbcli.configure(password)
+    if databases:
+        while True:
+            password = getpass.getpass('Password: ')
+            dbcli.configure(password)
 
-        if databases:
             test_db = databases[0].replace('.cookiedb', '')
             
             try:
                 dbcli.execute(f'db.open("{test_db}")')
             except cookiedb.exceptions.InvalidDatabaseKeyError:
-                print('\033[32mWrong password, try again\033[m')
+                print('\033[31mWrong password, try again\033[m')
             else:
                 break
-        else:
-            break
+    else:
+        print('Set a password for access to the database')
+        password = getpass.getpass('Password: ')
+        
+        while True:
+            confirm_password = getpass.getpass('Confirm password: ')
+            if password != confirm_password:
+                print('\033[31mPasswords are not equal. Try again\033[m')
+            else:
+                break
+
+        dbcli.configure(password)
