8be41ce032b813997fef56a55302b38360df145f
Jaedson Silva <contacanal798@gmail.com>
2022-08-12 09:51:24 -0300

    changing variable name to "databases"

--- cookiedb-cli/main.py
+++ cookiedb-cli/main.py
@@ -12,14 +12,14 @@ from cryptography import fernet
 class CookieDBCLI(object):
     def __init__(self):
         self._home_user = pathlib.Path.home()
-        self._database_dir_path = os.path.join(self._home_user, '.cookiedb')
+        self._databases_dir_path = os.path.join(self._home_user, '.cookiedb')
 
     def get_database(self) -> list:
         if not os.path.isdir(self._database_dir_path):
-            os.mkdir(self._database_dir_path)
-            database = []
+            os.mkdir(self._databases_dir_path)
+            databases = []
         else:
             listdir = os.listdir(self._database_dir_path)
-            database = [db for db in listdir if db.endswith('.cookiedb')]
+            databases = [db for db in listdir if db.endswith('.cookiedb')]
 
-        return database
+        return databases
