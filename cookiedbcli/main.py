0b3c643bb054ea2bad4554f380bbb062577ef251
Jaedson Silva <contacanal798@gmail.com>
2022-08-12 13:05:54 -0300

    adding KeyboardInterrupt exception handler

--- cookiedbcli/main.py
+++ cookiedbcli/main.py
@@ -12,31 +12,35 @@ def main():
     dbcli = CookieDBCLI()
     databases = dbcli.get_databases()
 
-    if databases:
-        while True:
+    try:
+        if databases:
+            while True:
+                password = getpass.getpass('Password: ')
+                dbcli.configure(password)
+
+                test_db = databases[0].replace('.cookiedb', '')
+                
+                try:
+                    dbcli.execute(f'db.open("{test_db}")')
+                except cookiedb.exceptions.InvalidDatabaseKeyError:
+                    print('\033[31mWrong password, try again\033[m')
+                else:
+                    break
+        else:
+            print('Set a password for access to the database')
             password = getpass.getpass('Password: ')
-            dbcli.configure(password)
-
-            test_db = databases[0].replace('.cookiedb', '')
             
-            try:
-                dbcli.execute(f'db.open("{test_db}")')
-            except cookiedb.exceptions.InvalidDatabaseKeyError:
-                print('\033[31mWrong password, try again\033[m')
-            else:
-                break
-    else:
-        print('Set a password for access to the database')
-        password = getpass.getpass('Password: ')
-        
-        while True:
-            confirm_password = getpass.getpass('Confirm password: ')
-            if password != confirm_password:
-                print('\033[31mPasswords are not equal. Try again\033[m')
-            else:
-                break
+            while True:
+                confirm_password = getpass.getpass('Confirm password: ')
+                if password != confirm_password:
+                    print('\033[31mPasswords are not equal. Try again\033[m')
+                else:
+                    break
 
-        dbcli.configure(password)
+            dbcli.configure(password)
+    except KeyboardInterrupt:
+        print('Bye.')
+        exit(0)
 
     print('Welcome to CookieDB CLI!')
     print('See the complete CookieDB documentation at https://github.com/jaedsonpys/cookiedb')
