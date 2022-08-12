013247f5d4a116af4277efec124a4b58a08a1b19
Jaedson Silva <contacanal798@gmail.com>
2022-08-12 09:47:11 -0300

    adding "get_database" method in CookieDBCLI

--- cookiedb-cli/main.py
+++ cookiedb-cli/main.py
@@ -12,4 +12,14 @@ from cryptography import fernet
 class CookieDBCLI(object):
     def __init__(self):
         self._home_user = pathlib.Path.home()
-        self._databases_dir_path = os.path.join(self._home_user, '.cookiedb')
+        self._database_dir_path = os.path.join(self._home_user, '.cookiedb')
+
+    def get_database(self) -> list:
+        if not os.path.isdir(self._database_dir_path):
+            os.mkdir(self._database_dir_path)
+            database = []
+        else:
+            listdir = os.listdir(self._database_dir_path)
+            database = [db for db in listdir if db.endswith('.cookiedb')]
+
+        return database
